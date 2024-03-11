d52103e5e401368d67fad7a96a28555c8589c5c3
slash128 <jg@granatlabs.com>
2024-03-11 15:53:44 -0700

    PCB updates

---
+++ Software/DC32_Cnet_Badge/Debug/Adafruit_NeoPixel/kendyte_k210.c.d
@@ -0,0 +1,2 @@
+C:\\Users\\jgranat\\AppData\\Local\\Temp\\VMBuilds\\DC32_Cnet_Badge\\esp32_esp32\\Debug\\Adafruit_NeoPixel\\kendyte_k210.c.o: \
+ c:\users\jgranat\my\ drive\dev\arduino\libraries\adafruit_neopixel\kendyte_k210.c
